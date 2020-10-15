500345c5c6673f09cb55420511e6bd57c2072510
Joshua Packer <jpacker@redhat.com>
2020-10-15 01:05:49 -0400

    Updates for my Fork

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,10 +4,8 @@ GH_URL=$1
 GH_PATH=$2
 NAMESPACE=$3
 
-URL_CFG=$(cat "url_template.json" | sed "s%##GH_URL##%${1:-https://github.com/open-cluster-management/policy-collection.git}%g")
+URL_CFG=$(cat "url_template.json" | sed "s%##GH_URL##%${1:-https://github.com/jnpacker/policy-collection.git}%g")
 echo "$URL_CFG" > url_patch.json
-PATH_CFG=$(cat "path_template.json" | sed "s/##GH_PATH##/${2:-stable}/g")
-echo "$PATH_CFG" > dir_patch.json
 CHANNEL_CFG=$(cat "channel_template.json" | sed "s/##NAMESPACE##/${3:-policies}/g")
 echo "$CHANNEL_CFG" > channel_patch.json
 KUST_CFG=$(cat "kustomization_template.yaml" | sed "s/##NAMESPACE##/${3:-policies}/g")
@@ -16,7 +14,6 @@ echo "$KUST_CFG" > kustomization.yaml
 kubectl kustomize . > resources.yaml
 kubectl apply -f resources.yaml
 
-rm dir_patch.json
 rm url_patch.json
 rm channel_patch.json
 rm kustomization.yaml
